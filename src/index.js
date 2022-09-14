a1b5b95ed274feadbc6af54fdfd67143427cabe3
amrit-dev <amrit.snr1995@gmail.com>
2022-09-14 13:26:16 +0900

    Created webpack for frontend Development setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles/main.scss";
+import generateJoke from "./generateJoke";
+import laughing from "./assets/laughing.svg";
+const laughImg = document.getElementById("laughImg");
+laughImg.src = laughing;
+
+const jokeBtn = document.getElementById('jokeBtn')
+jokeBtn.addEventListener('click',generateJoke)
+generateJoke()
